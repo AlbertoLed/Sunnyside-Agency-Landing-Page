8f91c4fa89a2ff45f57fce2980705c913969cc58
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-06-14 12:41:37 -0600

    Testimonials section container added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header.jsx'
 import Info from './Info.jsx'
 import { clientTestimonials } from '../data.js'
 import Testimonial from './Testimonial.jsx'
+import './App.css'
 
 function App() {
 
@@ -19,7 +20,10 @@ function App() {
       <main>
         <Info />
         {/* testimonials */}
-        {testimonials}
+        <section className='testimonials-section'>
+          <h2 className='testimonials-section__title'>Client testimonials</h2>
+          {testimonials}
+        </section>
         {/* photos */}
 
       </main>
