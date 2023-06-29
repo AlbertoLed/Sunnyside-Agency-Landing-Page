7c632446a542156b8d90d531fd258515f4086e99
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-06-29 16:14:01 -0600

    Add nav for desktop view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,13 @@ function Header() {
         <header className='header'>
             <nav className='nav'>
                 <img className='nav__logo' src={sunnysideLogo} alt="Sunnyside logo" />
-                <img className='nav__btn' src={navBtn} />
+                <ul className='desktop-nav'>
+                    <li className='desktop-nav__btn desktop-nav__btn--menu'>About</li>
+                    <li className='desktop-nav__btn desktop-nav__btn--menu'>Services</li>
+                    <li className='desktop-nav__btn desktop-nav__btn--menu'>Projects</li>
+                    <li className='desktop-nav__btn desktop-nav__btn--contact'>Contact</li>
+                </ul>
+                <img className='mobile-nav-launcher' src={navBtn} />
             </nav>
             <div className='header__content'>
                 <h1 className='header__title'>We are creatives</h1>
