008398c668c9d80fab8022325e6b47138a9f79ce
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-07-03 17:02:43 -0600

    Add animation to the textcard components the first time they appears on the screen

--- src/components/Textcard.jsx
+++ src/components/Textcard.jsx
@@ -1,8 +1,18 @@
-import React from "react"
+import React, { useRef } from "react"
 import './Textcard.css'
 
 function Textcard(props) {
     const {title, paragraph, color} = props
+
+    const [intersectionCount, setIntersectionCount] = React.useState(0)
+    const ref = React.useRef()
+    React.useEffect(() => {
+        console.log('use effect')
+        const oberver = new IntersectionObserver(() => {
+            setIntersectionCount(prev => prev + 1)
+        })
+        oberver.observe(ref.current)
+    }, [])
 
     const anchorColor = color => {
         if(color === 'yellow') {
@@ -14,7 +24,11 @@ function Textcard(props) {
     }
 
     return(
-        <div className={`card ${color === 'yellow' ? `card--left` :  `card--right`}`}>
+        <div 
+        className={`card 
+            ${color === 'yellow' ? `card--left` :  `card--right`}
+            ${intersectionCount >= 2 && `card--animation`}`}
+        ref={ref}>
             <h2 className='card__title'>{title}</h2>
             <p className='card__paragraph'>{paragraph}</p>
             <p className={`anchor__text ${anchorColor(color)}`}>Learn more</p>
