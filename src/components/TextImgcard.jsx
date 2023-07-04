bb39e60971cee4d4c25f34b334b6d1b68e9ff4a9
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-07-03 18:07:47 -0600

    Add animation to the textimgcard components the first time they appears on the screen

--- src/components/TextImgcard.jsx
+++ src/components/TextImgcard.jsx
@@ -4,6 +4,15 @@ import './TextImgcard.css'
 function TextImgcard(props) {
     const { title, paragraph, img, textcolor } = props
 
+    const [intersectionCount, setIntersectionCount] = React.useState(0)
+    const ref = React.useRef()
+    React.useEffect(() => {
+        const observe = new IntersectionObserver(() => {
+            setIntersectionCount(prev => prev + 1)
+        })
+        observe.observe(ref.current)
+    }, [])
+
     let txtcolorClass;
     if(textcolor === 'cyan') {
         txtcolorClass = 'img-card__text--cyan'
@@ -14,7 +23,9 @@ function TextImgcard(props) {
     const imgClass = img === 'image-graphic-design.jpg' ? 'img-card--graphic' : 'img-card--photography'
 
     return (
-        <div className={`img-card ${imgClass}`}>
+        <div 
+            className={`img-card ${imgClass} ${intersectionCount >= 2 && `img-card--animation`}`}
+            ref={ref}>
             <div className='img-card__text'>
                 <h2 className={`img-card__title ${txtcolorClass}`}>{title}</h2>
                 <p className={`img-card__paragraph ${txtcolorClass}`}>{paragraph}</p>
